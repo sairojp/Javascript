8b345ac6ac335c4edeb4e66e60ad8f153381212e
N0t0p <sairoj25@gmail.com>
2023-07-06 22:22:07 +0545

    using fetch api to learn about promise

---
+++ Learning JS/Other-topics/Promise/Promise1/index.js
@@ -0,0 +1,16 @@
+let dogAPI = "https://dog.ceo/api/breeds/image/random" ;
+
+//fetch is a promise which resolves or doesnot respond 
+//it responds with response object which itself isnot json so use json() method for conversion
+// json itself is promise which is resolved by chaining then again 
+fetch(dogAPI)
+.then(response => response.json())
+.then(json => {
+  console.log(json);
+  document.getElementById('image').src = json.message;
+})
+.catch(err => console.log(err)); 
+
+  
+
+ 
