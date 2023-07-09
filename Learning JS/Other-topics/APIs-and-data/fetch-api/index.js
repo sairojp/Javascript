c2461a96662ffa67263c76669b2218e0a5b489cc
N0t0p <sairoj25@gmail.com>
2023-07-09 20:21:16 +0545

    Using async await with fetch

--- Learning JS/Other-topics/APIs-and-data/fetch-api/index.js
+++ Learning JS/Other-topics/APIs-and-data/fetch-api/index.js
@@ -4,15 +4,29 @@
 // make the corresponding dom element
 
 console.log('about to fetch image')
-fetch('user.jpg')
-.then(response => {
-  console.log(response);
-  return response.blob(); 
-})
-.then(blob => {
-  console.log(blob);
-  return document.getElementById('image').src = URL.createObjectURL(blob);
-})
+
+showImage()
 .catch(err => {
   console.error(err);
 })
+
+
+async function showImage(){
+  let response = await fetch('user.jpg');
+  let blob = await response.blob();
+  document.getElementById('image').src = URL.createObjectURL(blob);
+}
+
+
+// fetch('user.jpg')
+// .then(response => {
+//   console.log(response);
+//   return response.blob(); 
+// })
+// .then(blob => {
+//   console.log(blob);
+//   return document.getElementById('image').src = URL.createObjectURL(blob);
+// })
+// .catch(err => {
+//   console.error(err);
+// })
